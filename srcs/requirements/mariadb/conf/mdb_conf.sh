3c0beaee3c1e82eeefc5f49b8f146acfb19d436b
alexlimfl <alexlimfl23@gmail.com>
2024-09-19 20:31:01 +0800

    up

--- srcs/requirements/mariadb/conf/mdb_conf.sh
+++ srcs/requirements/mariadb/conf/mdb_conf.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#---------------------------------------------------mariadb start---------------------------------------------------#
-
+# Start MariaDB service
 service mariadb start
 sleep 5
 
-#---------------------------------------------------mariadb config---------------------------------------------------#
-
+# Create database and user
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
-#---------------------------------------------------mariadb restart---------------------------------------------------#
-
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+# Restart MariaDB in safe mode (or directly run mysqld)
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+mysqld --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
